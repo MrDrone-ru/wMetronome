e48354611bce71b12776463de54fdfdfb1fcad56
Andrew <andrew07iv@ya.ru>
2023-10-29 20:00:39 +0300

    Updated to meet Chromium's requirements for AudioContext and workers. Now metronome works without web server.

--- assets/js/metronome.js
+++ assets/js/metronome.js
@@ -1,8 +1,8 @@
 var audioContext = null;
-var isPlaying = false;      // Are we currently playing?
-var startTime;              // The start time of the entire sequence.
-var currentTwelveletNote;        // What note is currently last scheduled?
-var tempo = 120.0;          // tempo (in beats per minute)
+var isPlaying = false; // Are we currently playing?
+var startTime; // The start time of the entire sequence.
+var currentTwelveletNote; // What note is currently last scheduled?
+var tempo = 120.0; // tempo (in beats per minute)
 var meter = 4;
 var masterVolume = 0.5;
 var accentVolume = 1;
@@ -10,109 +10,142 @@ var quarterVolume = 0.75;
 var eighthVolume = 0;
 var sixteenthVolume = 0;
 var tripletVolume = 0;
-var lookahead = 25.0;       // How frequently to call scheduling function
-                            //(in milliseconds)
-var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
-                            // This is calculated from lookahead, and overlaps
-                            // with next interval (in case the timer is late)
-var nextNoteTime = 0.0;     // when the next note is due.
-var noteLength = 0.05;      // length of "beep" (in seconds)
-var notesInQueue = [];      // the notes that have been put into the web audio,
-                            // and may or may not have played yet. {note, time}
-var timerWorker = null;     // The Web Worker used to fire timer messages
+var lookahead = 25.0; // How frequently to call scheduling function
+//(in milliseconds)
+var scheduleAheadTime = 0.1; // How far ahead to schedule audio (sec)
+// This is calculated from lookahead, and overlaps
+// with next interval (in case the timer is late)
+var nextNoteTime = 0.0; // when the next note is due.
+var noteLength = 0.05; // length of "beep" (in seconds)
+var notesInQueue = []; // the notes that have been put into the web audio,
+// and may or may not have played yet. {note, time}
+var timerWorker = null; // The Web Worker used to fire timer messages
 
 function maxBeats() {
-  var beats = (meter * 12);
-  return beats;
+    var beats = (meter * 12);
+    return beats;
 }
 
 function nextTwelvelet() {
-  var secondsPerBeat = 60.0 / tempo;
-  nextNoteTime += 0.08333 * secondsPerBeat;    // Add beat length to last beat time
-  currentTwelveletNote++;    // Advance the beat number, wrap to zero
+    var secondsPerBeat = 60.0 / tempo;
+    nextNoteTime += 0.08333 * secondsPerBeat; // Add beat length to last beat time
+    currentTwelveletNote++; // Advance the beat number, wrap to zero
 
-  if (currentTwelveletNote == maxBeats()) {
-    currentTwelveletNote = 0;
-  }
+    if (currentTwelveletNote == maxBeats()) {
+        currentTwelveletNote = 0;
+    }
 }
 
 function calcVolume(beatVolume) {
-  return (beatVolume * masterVolume);
+    return (beatVolume * masterVolume);
 }
 
 function scheduleNote(beatNumber, time) {
-  // push the note on the queue, even if we're not playing.
-  notesInQueue.push({ note: beatNumber, time: time });
+    // push the note on the queue, even if we're not playing.
+    notesInQueue.push({
+        note: beatNumber,
+        time: time
+    });
 
-  // create oscillator & gainNode & connect them to the context destination
-  var osc = audioContext.createOscillator();
-  var gainNode = audioContext.createGain();
+    // create oscillator & gainNode & connect them to the context destination
+    var osc = audioContext.createOscillator();
+    var gainNode = audioContext.createGain();
 
-  osc.connect(gainNode);
-  gainNode.connect(audioContext.destination);
+    osc.connect(gainNode);
+    gainNode.connect(audioContext.destination);
 
-  if (beatNumber % maxBeats() === 0) {
-    if (accentVolume > 0.25) {
-      osc.frequency.value = 880.0;
-      gainNode.gain.value = calcVolume(accentVolume);
+    if (beatNumber % maxBeats() === 0) {
+        if (accentVolume > 0.25) {
+            osc.frequency.value = 880.0;
+            gainNode.gain.value = calcVolume(accentVolume);
+        } else {
+            osc.frequency.value = 440.0;
+            gainNode.gain.value = calcVolume(quarterVolume);
+        }
+    } else if (beatNumber % 12 === 0) { // quarter notes = medium pitch
+        osc.frequency.value = 440.0;
+        gainNode.gain.value = calcVolume(quarterVolume);
+    } else if (beatNumber % 6 === 0) {
+        osc.frequency.value = 440.0;
+        gainNode.gain.value = calcVolume(eighthVolume);
+    } else if (beatNumber % 4 === 0) {
+        osc.frequency.value = 300.0;
+        gainNode.gain.value = calcVolume(tripletVolume);
+    } else if (beatNumber % 3 === 0) { // other 16th notes = low pitch
+        osc.frequency.value = 220.0;
+        gainNode.gain.value = calcVolume(sixteenthVolume);
     } else {
-      osc.frequency.value = 440.0;
-      gainNode.gain.value = calcVolume(quarterVolume);
+        gainNode.gain.value = 0; // keep the remaining twelvelet notes inaudible
     }
-  } else if (beatNumber % 12 === 0) {   // quarter notes = medium pitch
-    osc.frequency.value = 440.0;
-    gainNode.gain.value = calcVolume(quarterVolume);
-  } else if (beatNumber % 6 === 0) {
-    osc.frequency.value = 440.0;
-    gainNode.gain.value = calcVolume(eighthVolume);
-  } else if (beatNumber % 4 === 0) {
-    osc.frequency.value = 300.0;
-    gainNode.gain.value = calcVolume(tripletVolume);
-  } else if (beatNumber % 3 === 0 ) {                    // other 16th notes = low pitch
-    osc.frequency.value = 220.0;
-    gainNode.gain.value = calcVolume(sixteenthVolume);
-  } else {
-    gainNode.gain.value = 0;   // keep the remaining twelvelet notes inaudible
-  }
 
-  osc.start(time);
-  osc.stop(time + noteLength);
+    osc.start(time);
+    osc.stop(time + noteLength);
 }
 
 function scheduler() {
-  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
-    scheduleNote( currentTwelveletNote, nextNoteTime );
-    nextTwelvelet();
-  }
+    while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
+        scheduleNote(currentTwelveletNote, nextNoteTime);
+        nextTwelvelet();
+    }
 }
 
 function play() {
-  isPlaying = !isPlaying;
-
-  if (isPlaying) {
-    currentTwelveletNote = 0;
-    nextNoteTime = audioContext.currentTime;
-    timerWorker.postMessage("start");
-    document.getElementById("play-icon").innerHTML = "pause";
-  } else {
-    timerWorker.postMessage("stop");
-    document.getElementById("play-icon").innerHTML = "play_arrow";
-  }
-}
-
-function init(){
-  audioContext = new AudioContext();
-  timerWorker = new Worker("assets/js/worker.js");
-
-  timerWorker.onmessage = function(e) {
-    if (e.data == "tick") {
-      scheduler();
+    isPlaying = !isPlaying;
+    init();
+    if (isPlaying) {
+        currentTwelveletNote = 0;
+        nextNoteTime = audioContext.currentTime;
+        timerWorker.postMessage("start");
+        document.getElementById("play-icon").innerHTML = "pause";
     } else {
-      console.log("message: " + e.data);
+        timerWorker.postMessage("stop");
+        document.getElementById("play-icon").innerHTML = "play_arrow";
     }
-  };
-
-  timerWorker.postMessage({"interval":lookahead});
 }
+function workerF() {
+    var timerID = null;
+    var interval = 100;
 
-window.addEventListener("load", init );
+    self.onmessage = function (e) {
+        if (e.data == "start") {
+            console.log("starting");
+            timerID = setInterval(function () {
+                postMessage("tick");
+            }, interval);
+        } else if (e.data.interval) {
+            console.log("setting interval");
+            interval = e.data.interval;
+            console.log("interval=" + interval);
+            if (timerID) {
+                clearInterval(timerID);
+                timerID = setInterval(function () {
+                    postMessage("tick");
+                }, interval);
+            }
+        } else if (e.data == "stop") {
+            console.log("stopping");
+            clearInterval(timerID);
+            timerID = null;
+        }
+    };
+
+    postMessage('hi there');
+}
+function init() {
+    if (!audioContext) {
+        audioContext = new AudioContext();
+        timerWorker = new Worker(URL.createObjectURL(new Blob(["(" + workerF.toString() + ")()"], {type: 'text/javascript'})));
+
+        timerWorker.onmessage = function (e) {
+            if (e.data == "tick") {
+                scheduler();
+            } else {
+                console.log("message: " + e.data);
+            }
+        };
+
+        timerWorker.postMessage({
+            "interval": lookahead
+        });
+    }
+}
